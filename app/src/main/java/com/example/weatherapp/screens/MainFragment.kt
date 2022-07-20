0661559de30fdae0133ccf8c7b50ec3ab6c2cfe3
FrikoGad <goliaf551@mail.ru>
2022-07-20 22:00:23 +0300

    request

--- app/src/main/java/com/example/weatherapp/screens/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/screens/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.weatherapp.screens
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,9 +13,13 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.weatherapp.R
 import com.example.weatherapp.adapters.ViewPagerAdapter
 import com.example.weatherapp.databinding.FragmentMainBinding
+import com.example.weatherapp.utils.API_KEY
 import com.example.weatherapp.utils.isPermissionGranted
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -64,6 +69,22 @@ class MainFragment : Fragment() {
             permissionListener()
             pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
+    }
+
+    private fun requestWeatherData(city: String) {
+        val url = "https://api.weatherapi.com/v1/forecast.json?key=$API_KEY&q=$city&days=5&aqi=no&alerts=no"
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                result -> Log.d("MyLog", "Result: $result")
+            },
+            {
+                error -> Log.d("MyLog", "Error: $error")
+            }
+        )
+        queue.add(request)
     }
 
     companion object {
---
+++ app/src/main/java/com/example/weatherapp/utils/Const.kt
@@ -0,0 +1,3 @@
+package com.example.weatherapp.utils
+
+const val API_KEY = "f1e461295192489aa71174448220607"
