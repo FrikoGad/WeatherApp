9ea3bbdaa02b00bbeeb7c9a1502dfde502656666
FrikoGad <goliaf551@mail.ru>
2022-09-21 22:16:35 +0300

    Search by city

--- app/src/main/java/com/example/weatherapp/screens/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/screens/MainFragment.kt
@@ -80,6 +80,15 @@ class MainFragment : Fragment() {
             tabLayout.selectTab(tabLayout.getTabAt(0))
             checkLocation()
         }
+        ibSearch.setOnClickListener {
+            DialogManager.searchByNameDialog(requireContext(), object : DialogManager.Listener {
+                override fun onClick(name: String?) {
+                    if (name != null) {
+                        requestWeatherData(name)
+                    }
+                }
+            })
+        }
     }
 
     private fun checkLocation() {
@@ -87,7 +96,7 @@ class MainFragment : Fragment() {
             getLocation()
         } else {
             DialogManager.locationSettingsDialog(requireContext(), object : DialogManager.Listener {
-                override fun onClick() {
+                override fun onClick(name: String?) {
                     startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
                 }
 
